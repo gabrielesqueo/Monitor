9ba759ea6bae358ecce8501a17ad638559e1a8da
Nicola Menolascina <nicola.menola1505@gmail.com>
2022-01-20 17:12:25 +0100

    SoleBox aggiornato

---
+++ monitor/SoleBox Monitor/discord.py
@@ -0,0 +1,23 @@
+import requests
+import datetime
+import json
+def discord_webhook(product):
+    webhook='https://discord.com/api/webhooks/932919093105917952/667uWD0eipNzB1TWjj11tGDj25SejJCdQVvGlHDMPh5h3e0FufHvzDLALYJ3N0Js7G1p'
+    data = {
+        "username": 'ZalandoMonitor',
+        "avatar_url": 'https://www.pngitem.com/pimgs/m/122-1223088_one-bot-discord-avatar-hd-png-download.png',
+        "embeds": [{
+            "title": product[0],
+            "url": product[1],
+            "thumbnail": {"url": product[4]},
+            "color": int('15258703'),
+            "footer": {"text": "Zalando Monitor"},
+            "timestamp": str(datetime.datetime.now()),
+            "fields": [
+                {"name": "Brand", "value": str(product[2])},
+                {"name": "Price", "value": str(product[3])}
+            ]
+        }]
+    }
+
+    result = requests.post(webhook,data=json.dumps(data), headers={"Content-Type": "application/json"})
